3fdcb5b72a3f6226cecfc8a214e2c7c489da0361
AChudzynska <230169@student.pwr.edu.pl>
2017-01-31 12:21:27 +0100

    projekt 2
    kompletny

---
+++ WeatherApplication/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace WeatherApplication
+{
+    partial class ApplicationForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pogodaTC = new System.Windows.Forms.TabControl();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.miastoCB = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // pogodaTC
+            // 
+            this.pogodaTC.Location = new System.Drawing.Point(12, 121);
+            this.pogodaTC.Name = "pogodaTC";
+            this.pogodaTC.SelectedIndex = 0;
+            this.pogodaTC.Size = new System.Drawing.Size(327, 212);
+            this.pogodaTC.TabIndex = 3;
+            this.pogodaTC.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(221, 25);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pogoda dla miasta: ";
+            // 
+            // miastoCB
+            // 
+            this.miastoCB.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.miastoCB.FormattingEnabled = true;
+            this.miastoCB.Location = new System.Drawing.Point(16, 43);
+            this.miastoCB.Name = "miastoCB";
+            this.miastoCB.Size = new System.Drawing.Size(200, 32);
+            this.miastoCB.TabIndex = 5;
+            this.miastoCB.Text = "Wybierz miasto...";
+            this.miastoCB.SelectedIndexChanged += new System.EventHandler(this.miastoCB_SelectedIndexChanged);
+            // 
+            // ApplicationForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(350, 348);
+            this.Controls.Add(this.miastoCB);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pogodaTC);
+            this.Name = "ApplicationForm";
+            this.Text = "Pogodynka";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl pogodaTC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ComboBox miastoCB;
+    }
+}
+
---
+++ WeatherApplication/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Collections;
+
+
+namespace WeatherApplication
+{
+    interface IObserwowany
+    {
+        void dodajObserwatora(IObserwator o);
+        void usunObserwatora(IObserwator o);
+        void powiadomObserwatorow(XmlDocument xmlDok);
+    }
+
+    interface IObserwator
+    {
+        void aktualizujPogode(XmlDocument xmlDok);
+    }
+
+    public partial class ApplicationForm : Form
+    {
+        public ApplicationForm()
+        {
+            InitializeComponent();
+
+            miastoCB.Items.Add("Dzialdowo");
+            miastoCB.Items.Add("Wroclaw");
+            miastoCB.Items.Add("Warszawa");    
+			//miastoCB.Items.Add("");
+        }
+
+        private void miastoCB_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Pogodynka pogodynka = new Pogodynka(); 
+            Aplikacja aplikacja = new Aplikacja(pogodynka); 
+            XmlDocument xmlPogoda = new XmlDocument();
+
+            pogodynka.dodajObserwatora(aplikacja);
+
+            pogodynka.odczytajPogode(miastoCB.SelectedItem.ToString());
+            xmlPogoda = aplikacja.xmlPogoda;
+
+            pogodaTC.TabPages.Clear();
+
+            XmlNodeList xmlNodeList = xmlPogoda.GetElementsByTagName("forecastday");
+            int i = 0;
+            foreach (XmlNode xmlNode in xmlNodeList)
+            {
+                pogodaTC.TabPages.Add(xmlNode.SelectSingleNode("date/weekday").InnerText);
+
+                Label data = new Label();
+                data.Text = xmlNode.SelectSingleNode("date/day").InnerText + "." +
+                             xmlNode.SelectSingleNode("date/month").InnerText + "." +
+                             xmlNode.SelectSingleNode("date/year").InnerText;
+                pogodaTC.TabPages[i].Controls.Add(data);
+                pogodaTC.TabPages[i].Controls[0].Location = new Point(10, 10);
+
+                Label warunki = new Label();
+                warunki.Text = "warunki: " + xmlNode.SelectSingleNode("conditions").InnerText;
+                warunki.Width = 200;
+                warunki.Height = 20;
+                pogodaTC.TabPages[i].Controls.Add(warunki);
+                pogodaTC.TabPages[i].Controls[1].Location = new Point(10, 40);
+
+                Label temperatura = new Label();
+                temperatura.Text = "temp. (min/max): " +
+                                   xmlNode.SelectSingleNode("low/celsius").InnerText + @" / " +
+                                   xmlNode.SelectSingleNode("high/celsius").InnerText + @" C";
+                temperatura.Width = 200;
+                temperatura.Height = 20;
+                pogodaTC.TabPages[i].Controls.Add(temperatura);
+                pogodaTC.TabPages[i].Controls[2].Location = new Point(10, 60);
+
+                Label wiatr = new Label();
+                wiatr.Text = "wiatr: do " +
+                             xmlNode.SelectSingleNode("maxwind/kph").InnerText + @" km/h, " +
+                             xmlNode.SelectSingleNode("maxwind/dir").InnerText;
+                wiatr.Width = 200;
+                wiatr.Height = 20;
+                pogodaTC.TabPages[i].Controls.Add(wiatr);
+                pogodaTC.TabPages[i].Controls[3].Location = new Point(10, 80);
+
+                PictureBox picture = new PictureBox();
+                picture.ImageLocation = xmlNode.SelectSingleNode("icon_url").InnerText;
+                pogodaTC.TabPages[i].Controls.Add(picture);
+                pogodaTC.TabPages[i].Controls[4].Location = new Point(220, 10);
+
+                i++;
+            }
+            pogodaTC.Visible = true;
+        }
+
+        
+    }
+
+    
+    class Pogodynka : IObserwowany
+    {
+        private ArrayList obserwatorzy;
+        private XmlDocument xmlDok;
+
+        public Pogodynka()
+        {
+            obserwatorzy = new ArrayList();
+            xmlDok = new XmlDocument();
+        }
+
+        public void dodajObserwatora(IObserwator o)
+        {
+            obserwatorzy.Add(o);
+        }
+
+        public void usunObserwatora(IObserwator o)
+        {
+            int indeks = obserwatorzy.IndexOf(o);
+            obserwatorzy.RemoveAt(indeks);
+        }
+
+        public void powiadomObserwatorow(XmlDocument xmlDok)
+        {
+            IObserwator[] tab = (IObserwator[])obserwatorzy.ToArray(typeof(IObserwator));
+            for (int i = 0; i < tab.Length; i++)
+            {
+                tab[i].aktualizujPogode(xmlDok);
+            }
+        }
+
+        public void odczytajPogode(string miasto)
+        {
+            
+            string url = @"http://api.wunderground.com/api/c72e58c3896a0ba8/forecast/lang:PL/q/Poland/" + miasto + ".xml";
+            xmlDok.Load(url);
+            powiadomObserwatorow(xmlDok);
+        }
+
+        public XmlDocument getResult()
+        {
+            return xmlDok;
+        }
+    }
+
+    
+    class Aplikacja: IObserwator
+    {
+        public XmlDocument xmlPogoda;
+        private Pogodynka pogodynka;
+
+        public Aplikacja(Pogodynka pogodynka)
+        {
+            xmlPogoda = new XmlDocument();
+            this.pogodynka = pogodynka;
+        }
+
+        public void aktualizujPogode(XmlDocument xmlDok)
+        {
+            xmlPogoda.LoadXml("<xmlPogoda></xmlPogoda>");
+            XmlNode node = xmlPogoda.ImportNode(xmlDok.SelectSingleNode("/response/forecast/simpleforecast/forecastdays"), true);
+            xmlPogoda.DocumentElement.AppendChild(node);
+        }
+
+
+        public void przestanObserwowac()
+        {
+            pogodynka.usunObserwatora(this);
+        }
+    }
+
+}
